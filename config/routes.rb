7b67edb3fcbfc1454bbfff3d4baf32a099b92ceb
Mini Smith <minismith742@gmail.com>
2022-08-30 18:01:03 +0100

    Made bookings connected to user and space

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users, only: [:show] do
-    resources :spaces, only: %i[new create]
+    resources :spaces, only: %i[new create] do
+      resources :bookings, only: %i[new create]
+    end
   end
   resources :spaces, only: %i[index show edit update destroy]
 end
