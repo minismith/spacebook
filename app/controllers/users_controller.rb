9d61a15bf06e27f33a4b6155a9808d0d13ea096c
Mini Smith <minismith742@gmail.com>
2022-08-30 10:39:28 +0100

    Actually fixed it this time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @spaces = Space.where(params[:id] == @user.id)
+    @spaces = Space.where(user_id: params[:id])
   end
 end
