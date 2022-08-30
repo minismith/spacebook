84461c942be7d6117481c0778a49a6fa628bce18
Mini Smith <minismith742@gmail.com>
2022-08-30 14:41:05 +0100

    saving changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.active_storage.replace_on_assign_to_many = false
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
