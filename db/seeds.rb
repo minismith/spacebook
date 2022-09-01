68b77e657748bcab6947fa0a79ac0ae547a5082b
leena aden <adenleena@gmail.com>
2022-09-01 17:08:21 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,55 +40,55 @@ space.save
 
 file = URI.open("https://images.squarespace-cdn.com/content/v1/5b4f07024eddec73df849c5b/1534849958994-0H294JKKHIWA7SNMWNAT/Labs+Camden+house+%C2%A9++LUZ-4.jpg?format=2500w")
 space = Space.new(name:Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: "")
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.tagvenue.com/blog/wp-content/uploads/2016/10/Devonshire-Terrace-01.jpg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.peerspace.com/resources/wp-content/uploads/2018/05/vfhik8o2ifudxhhvxhfn.jpg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHeBfi7fXqjTWtZiy0Hq-UPD7msPd2ih9QGC3hbVVaYmFY-DwnKki58c5_3sezbQIJg7c&usqp=CAU")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.paintworksbristol.co.uk/fileadmin/user_upload/Evoke-Event-Space_096_1200.jpg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPH-SdVpErAodvRZU-j33sZgZkuw8bnwYNiw&usqp=CAU")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.socialtables.com/wp-content/uploads/2018/06/5-1.jpg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.millbrook.co.uk/media/ftffitpy/smmt-drive-zero-corporate-event-millbrook-venues.jpg?anchor=center&mode=crop&width=705&height=636&rnd=132521724952500000")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/743/cached.offlinehbpl.hbpl.co.uk/news/OMC/queen_hoxton_london_event_venue_rooftop_outdoor_private_hire-20150617033823336.jpg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://www.uniquevenuesoflondon.co.uk/sites/default/files/styles/venue_cover/public/cover/Standard%20set%20up_0.jpg?itok=aolT5Ihg")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
 
 file = URI.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzhJH7iT7NpKkG59sVtx2JGX6m7jDsDn_4RA&usqp=CAU")
 space = Space.new(name: Faker::App.name, description: Faker::Lorem.sentence(word_count: 10), address: Faker::Address.full_address, price_per_day: rand(10..500), user: User.first)
-space.photo.attach(io: file, filename: "nes.png", content_type: "image/jpg")
+space.photos.attach(io: file, filename: "nes.png", content_type: "image/jpg")
 space.save
